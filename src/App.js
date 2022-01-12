ee7536671d3e6a971bb127f323269224964549d3
Jiasheng Lu <jonsnowljs@gmail.com>
2022-01-11 23:40:27 -0700

    use jsx syntax to rewrite

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
+const pets = [
+  { name: "Jiman", animal: "Dog", breed: "Mix" },
+  { name: "Pepper", animal: "Dog", breed: "Havanese" },
+  { name: "Luna", animal: "Dog", breed: "Wheaten" },
+];
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Jiman", animal: "Dog", breed: "Mix" }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Wheaten" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      {pets.map((pet) => {
+        return (
+          <Pet name={pet.name} animal={pet.animal} breed={pet.breed}></Pet>
+        );
+      })}
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
