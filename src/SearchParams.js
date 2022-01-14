f95269ebfebab7f3308923081287905a966c27b4
Jiasheng Lu <jonsnowljs@gmail.com>
2022-01-13 21:09:20 -0700

    add option

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Calgary, AB");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
@@ -14,6 +17,22 @@ const SearchParams = () => {
             value={location.toUpperCase()}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
