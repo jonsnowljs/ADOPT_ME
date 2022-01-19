e071be0302d56a1d842fae09389bf0a797ea9411
Jiasheng Lu <1473875731@qq.com>
2022-01-18 20:49:48 -0700

    add image to the detail page

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   state = { loading: true };
@@ -24,9 +25,11 @@ class Details extends Component {
       return <h2>Loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
