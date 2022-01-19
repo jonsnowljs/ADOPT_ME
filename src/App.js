32bf7a7a3c06c9a2092e554c19eb5e5efdd86c6b
Jiasheng Lu <1473875731@qq.com>
2022-01-18 19:37:42 -0700

    Add React Router to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>Hi this is me. To be add more Details.</h2>;
+};
+
+export default Details;
