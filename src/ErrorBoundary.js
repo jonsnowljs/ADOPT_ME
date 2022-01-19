9d1bdb02e311a8320e43569a44a35e2f577c9ed0
Jiasheng Lu <1473875731@qq.com>
2022-01-19 11:44:16 -0700

    add redirect

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,16 +1,23 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
   }
-  render() {
+  componentDidUpdate() {
     if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           There was an error with this listing. <Link to="/">Click here</Link>{" "}
