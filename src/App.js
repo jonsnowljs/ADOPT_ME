0f2a31fc20b6ac6d60fb40a4e86948e79057159c
Jiasheng Lu <1473875731@qq.com>
2022-01-18 19:24:23 -0700

    Add strict mode on app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 
@@ -11,4 +11,9 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
