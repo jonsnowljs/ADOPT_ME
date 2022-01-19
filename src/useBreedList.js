97f622ade9a34eff9524ff5a93cb37e02c8014a5
Jiasheng Lu <1473875731@qq.com>
2022-01-18 19:23:46 -0700

    fix the link bug on api

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -14,14 +14,13 @@ export default function useBreedList(animal) {
     } else {
       requestBreedList();
     }
+
     async function requestBreedList() {
       setBreedList([]);
-      setStatus("Loading");
-
+      setStatus("loading");
       const res = await fetch(
-        `http://pets-v2.dev-apis.com/pets?animal=${animal}`
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
       );
-
       const json = await res.json();
       localCache[animal] = json.breeds || [];
       setBreedList(localCache[animal]);
