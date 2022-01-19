8a09a1913e514d81d0361492747f51e34561dfc3
Jiasheng Lu <1473875731@qq.com>
2022-01-18 20:10:10 -0700

    implement details component in Class component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,39 @@
-const Details = () => {
-  return <h2>Hi this is me. To be add more Details.</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
