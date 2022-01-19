f3574014abc260656e52f2eaa7883a5b5ca926b5
Jiasheng Lu <1473875731@qq.com>
2022-01-18 19:02:50 -0700

    add return to make the result show

--- src/Results.js
+++ src/Results.js
@@ -7,15 +7,17 @@ const Results = ({ pets }) => {
         <h2>No Pets Found</h2>
       ) : (
         pets.map((pet) => {
-          <Pet
-            name={pet.name}
-            animal={pet.animal}
-            breed={pet.breed}
-            key={pet.id}
-            images={pet.images}
-            location={`${pet.city}, ${pet.state}`}
-            id={pet.id}
-          />;
+          return (
+            <Pet
+              name={pet.name}
+              animal={pet.animal}
+              breed={pet.breed}
+              key={pet.id}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
         })
       )}
     </div>
